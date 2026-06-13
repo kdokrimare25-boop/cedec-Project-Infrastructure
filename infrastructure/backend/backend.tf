b3dd708c71f18ba681c0c3e58119c52441784723
Kamini Dokrimare <kamini@gmail.com>
2026-06-13 13:21:23 +0530

    kaminid | Backend updated

--- infrastructure/backend/backend.tf
+++ infrastructure/backend/backend.tf
@@ -6,13 +6,9 @@
 
 terraform {
   backend "s3" {
-<<<<<<< HEAD
     bucket = "cdec-alpha-terraform-state-kaminid"
-=======
-    bucket = "cdec-alpha-terraform-state-atulyw"
->>>>>>> 5c6e21a1ed1760f6d27f354a8ddcf660221ee0d5
     key    = "backend/terraform.tfstate"
-    region = "eu-west-1"
+    region = "eu-north-1"
     #profile = "terraform-sessions"
 
   }
